36d5fa9b44718083fde18ffe43988a2b1a3311aa
glm <1162965098@qq.com>
2023-02-26 18:18:28 +0800

    111

---
+++ src/main/java/com/example/demozx/DemoZxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demozx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoZxApplication.class, args);
+    }
+
+}
